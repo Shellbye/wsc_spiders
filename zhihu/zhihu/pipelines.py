c2e476769e160dcc8b007c385eb7f97d5e74cfbf
shellbye <444854713@qq.com>
2014-12-23 08:22:29 +0800

    simplified scrapy and the original scrapy_mongodb.py

--- zhihu/zhihu/pipelines.py
+++ zhihu/zhihu/pipelines.py
@@ -4,8 +4,25 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from pymongo.mongo_client import MongoClient
+import settings
 
 
 class ZhihuPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+
+class MongoDBPipelineSimplified(object):
+
+    def __init__(self):
+        self.connection = MongoClient(settings.MONGODB_URI)
+        self.database = self.connection[settings.MONGODB_DATABASE]
+        self.collection = None
+
+    def process_item(self, item, spider):
+        self.collection = self.database[spider.name]
+        if not isinstance(item, list):
+            item = dict(item)
+        self.collection.insert(item, continue_on_error=True)
         return item
