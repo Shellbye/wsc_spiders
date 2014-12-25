374474c1b6f86b4fcfe745296247b37387022025
shellbye <444854713@qq.com>
2014-12-25 15:45:19 +0800

    refactored whole project

---
+++ wsc/settings.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for wsc project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'wsc'
+
+SPIDER_MODULES = ['wsc.spiders', 'zhihu.spiders', 'lagou.spiders']
+NEWSPIDER_MODULE = 'wsc.spiders'
+
+MONGODB_URI = 'mongodb://192.168.2.222:27017'
+MONGODB_DATABASE = 'scrapy'
+MONGODB_COLLECTION = 'wsc'
+
+ITEM_PIPELINES = {
+    'wsc.pipelines.MongoDBPipeline': 300,
+}
+
+LOG_FILE = 'wsc.log'
+LOG_LEVEL = 'INFO'
+
+COOKIES_ENABLES = True
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
+    'wsc.rotate_useragent.RotateUserAgentMiddleware': 400
+}
---
+++ zhihu/items.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ZhihuItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+class ZhiHuUserProfile(scrapy.Item):
+    user_data_id = scrapy.Field()
+    name = scrapy.Field()
+    url_name = scrapy.Field()
+    bio = scrapy.Field()
+    locations = scrapy.Field()
+    business = scrapy.Field()
+    business_topic_url = scrapy.Field()
+    gender = scrapy.Field()
+    employments = scrapy.Field()
+    educations = scrapy.Field()
+    description = scrapy.Field()
+    vote_count = scrapy.Field()
+    thank_count = scrapy.Field()
+    fav_count = scrapy.Field()
+    share_count = scrapy.Field()
+    weibo_url = scrapy.Field()
+    followee_count = scrapy.Field()
+    follower_count = scrapy.Field()
+    questions_count = scrapy.Field()
+    answers_count = scrapy.Field()
+    posts_count = scrapy.Field()
+    collections_count = scrapy.Field()
+    logs_count = scrapy.Field()
+    personal_page_view_count = scrapy.Field()
+    follow_columns_count = scrapy.Field()
+    follow_topics_count = scrapy.Field()
+    questions = scrapy.Field()
+    answers = scrapy.Field()
