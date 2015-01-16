059e90c833ebb5b38f80565df5c42b21aca8a3e4
shellbye <444854713@qq.com>
2015-01-16 11:58:41 +0800

    fix data script

--- mongo_data_fix.py
+++ mongo_data_fix.py
@@ -5,28 +5,28 @@ from wsc.settings import IP, DB
 
 zhihu_user_data_ids = MongoClient(IP, 27017)[DB]['zhihu_user_data_ids']
 user_profile = MongoClient(IP, 27017)[DB]['user_profile']
-data_ids = zhihu_user_data_ids.find({"fetched": True}).sort([("$natural", -1)])
-# print data_ids.count()
-count = 0
-for data_id in data_ids:
-    # print data_id['user_data_id']
-    # if count > 5:
-    #     break
-    user_data_id = data_id['user_data_id']
-    user = user_profile.find({"user_data_id": user_data_id})
-    c = user.count()
-    # print c
-    if c:
-        # print "pass..." + user_data_id
-        pass
-    else:
-        # print "delete." + user_data_id
-        zhihu_user_data_ids.update(data_id,
-                                   {
-                                       "$set":
-                                           {
-                                               "fetched": False
-                                           }
-                                   }
-        )
-    # count += 1
+
+
+def process():
+    count = 0
+    data_ids = zhihu_user_data_ids.find({"fetched": True, "crawled_successfully": False}).sort([("_id", -1)])
+    for data_id in data_ids:
+        print count
+        user_data_id = data_id['user_data_id']
+        user = user_profile.find({"user_data_id": user_data_id})
+        c = user.count()
+        print c
+        if c:
+            print "pass..." + user_data_id
+            pass
+        else:
+            print "delete." + user_data_id
+            zhihu_user_data_ids.update({"user_data_id": user_data_id},
+                                       {"$set": {"fetched": False}}
+            )
+        count += 1
+
+
+if __name__ == "__main__":
+    process()
+    print "leaving"
