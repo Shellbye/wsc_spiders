656dee5d24fce8f8f994f1570be489a0001ae1e7
shellbye <444854713@qq.com>
2015-01-27 14:37:51 +0800

    high_school and middle_school with their start

---
+++ start_high_school.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+__author__ = 'shellbye'
+import logging
+logging.basicConfig(filename='start.err.log', level=logging.DEBUG)
+
+from twisted.internet import reactor
+from scrapy.crawler import Crawler
+from scrapy import log, signals
+from zhongkao.spiders.high_school import HighSchoolSpider
+from scrapy.utils.project import get_project_settings
+
+
+def setup_spider():
+    spider = HighSchoolSpider()
+    settings = get_project_settings()
+    crawler = Crawler(settings)
+    crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
+    crawler.configure()
+    crawler.crawl(spider)
+    crawler.start()
+
+
+def start():
+    setup_spider()
+    logging.log(logging.INFO, "setup high_school")
+    log.start()
+    reactor.run()
+
+
+if __name__ == "__main__":
+    start()
