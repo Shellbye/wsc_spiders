16f3b9f31028398b927cb1f804dff464ffc4b5d0
shellbye <444854713@qq.com>
2015-01-28 09:45:36 +0800

    add libssl-dev in setup_env.sh and some package in pip required.txt

--- files/setup_env.sh
+++ files/setup_env.sh
@@ -9,6 +9,7 @@ sudo apt-get install -y libxml2
 sudo apt-get install -y libxslt-dev
 sudo apt-get install -y libffi-dev
 sudo apt-get install -y python-pip
+sudo apt-get install libssl-dev
 echo "apt-get ended..."
 echo "pip install started..."
 sudo pip install -r required.txt
