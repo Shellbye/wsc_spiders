b6b6c67f13c7002b3d87c5c3dea895f9f302fbc1
shellbye <444854713@qq.com>
2015-01-31 16:53:41 +0800

    page

---
+++ zhihu/spiders/page.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import json
+import urllib2
+
+_xsrf = 'f1460d2580fbf34ccd508eb4489f1097'
+url = 'http://www.zhihu.com/node/ProfileFolloweesListV2'
+offset = 20
+params = json.dumps(
+    {
+        "offset": str(offset),
+        "order_by": "created",
+        "hash_id": '18c79c6cc76ce8db8518367b46353a54'
+    }
+)
+post_params = {"params": params, "method": "next", "_xsrf": _xsrf}
+ret = urllib2.urlopen(url, post_params)
+print ret
